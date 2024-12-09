70a289ddc20927200b61299f86df70816d6217cf
Mary Manousou <mary@Marys-MacBook-Pro.local>
2024-12-09 19:24:29 +0200

    add simple contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-    
-  </div>
+  <form name="contact" netlify>
+    <p>
+      <label>Όνομα <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>Email <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label>Τηλέφωνο <input type="number" name="phone" /></label>
+    </p>
+    <p>
+      <button type="submit">Αποστολή</button>
+    </p>
+  </form>
 </template>
