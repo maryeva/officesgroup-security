685ed2d8c5222865ca84fb8e7f6894e1d4d01ac1
Mary Manousou <mary@Marys-MacBook-Pro.local>
2025-02-18 03:11:29 +0200

    brand offer and make footer minimal

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,40 +6,31 @@
   <Bounded
     as="footer"
     y-padding="sm"
-    class="bg-brand"
+    class="bg-white border-brand border-t-2 mt-16"
   >
-    <footer class="text-white">
-      <div class="grid grid-cols-3 gap-4 content-center">
+    <footer class="text-slate-800 pt-8">
+      <div class="grid grid-cols-2 gap-4 content-center">
         <div>
           <p class="font-bold mb-4 uppercase">Στοιχεια Επικοινωνιας</p>
           <ul>
             <li>
               <span>Τηλέφωνο: </span>
-              <a href="tel:+302109233888" class="hover:text-white">+30 (210) 92 33 888</a>
+              <a href="tel:+302109233888">+30 (210) 92 33 888</a>
             </li>
             <li class="mt-2">
               <span >Email: </span>
-              <a href="mailto:synergates@officesgroup.gr" class="hover:text-white">synergates@officesgroup.gr</a>
+              <a href="mailto:synergates@officesgroup.gr">synergates@officesgroup.gr</a>
             </li>
           </ul>
         </div>
         <div>
           <p class="font-bold mb-4 uppercase">Που θα μας βρειτε</p>
           <p>Ιωσήφ των Ρωγών 15 Αθήνα, Αττική, 117 43, Ελλάδα</p>
-          <GoogleMap class="mt-4"/>
-        </div>
-        <div>
-          <p class="font-bold mb-4 uppercase">Χρησιμοι Συνδεσμοι</p>
-          <ul>
-            <li>
-              <a href="https://www.officesservice.gr/" target="_blank" class="block">Officesservice.gr</a>
-              <a href="https://outsourcing.officesgroup.gr/" target="_blank" class="block mt-2">Officesgroup Outsourcing</a>
-            </li>
-          </ul>
+  
         </div>
       </div>
-      <div class="border-t-2 border-white mt-8">
-        <p class="mt-2 text-right">©{{ currentYear }} Officesgroup.gr</p>
+      <div class="border-t border-brand mt-8">
+        <p class="mt-2 text-left">Copyright © Office Services International {{ currentYear }}</p>
       </div>
     </footer>
   </Bounded>
