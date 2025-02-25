e479c0429ae2ec3ca65fa06b16821d1064acb064
Mary Manousou <mary@Marys-MacBook-Pro.local>
2025-02-25 22:25:36 +0200

    add footer icons

--- components/Footer.vue
+++ components/Footer.vue
@@ -11,19 +11,23 @@
     <footer class="text-slate-800 pt-8">
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 content-center">
         <div>
-          <p class="font-bold mb-4 uppercase">Που θα μας βρειτε</p>
-          <p>Ζαχία 1 Αθήνα, Αττική, 10445</p>
+          <p class="font-bold mb-2 uppercase">Που θα μας βρειτε</p>
+          <LocationIcon class="inline-block mr-[5px] text-brand"/>
+          <p class="inline-block">Ζαχία 1 Αθήνα, Αττική, 10445</p>
           
-          <p class="font-bold mt-16 mb-4 uppercase">Στοιχεια Επικοινωνιας</p>
+          <p class="font-bold mt-16 mb-2 uppercase">Στοιχεια Επικοινωνιας</p>
           <ul>
             <li>
-              <span>Τηλέφωνο: </span>
+              <PhoneIcon class="inline-block mr-[5px] text-brand"/>
               <a href="tel:+302109233888">+30 (210) 92 33 888</a>
             </li>
-            <li class="mt-8">
-              <span >Email: </span>
-              <a href="mailto:offices@officesservice.gr" class="block">offices@officesservice.gr</a>
-              <a href="mailto:synergates@officesgroup.gr" class="mt-2">synergates@officesgroup.gr</a>
+            <li class="mt-8 flex ">
+              <EmailIcon class="inline-block mr-[5px] text-brand"/>
+              <div class="inline-block ml-[5px]">
+                <a href="mailto:offices@officesservice.gr" class="block">offices@officesservice.gr</a>
+                <a href="mailto:synergates@officesgroup.gr" class="mt-2">synergates@officesgroup.gr</a>
+              </div>
+              
             </li>
           </ul>
         </div>
